2f497222d4d660e4771d270e143db0746948f878
sincze <sincze@gmail.com>
2019-08-25 10:00:44 +0200

    Add files via upload

---
+++ fakeDomoticz.py
@@ -0,0 +1,27 @@
+#
+#   Buienradar.nl Weather Lookup Plugin
+#
+#   Frank Fesevur, 2017
+#   https://github.com/ffes/domoticz-buienradar
+#
+#   About the weather service:
+#   https://www.buienradar.nl/overbuienradar/gratis-weerdata
+#
+#   Very simple module to make local testing easier
+#   It "emulates" Domoticz.Log() and Domoticz.Debug()
+#
+
+def Log(s):
+    print(s)
+
+def Debug(s):
+    print(s)
+
+def Error(s):
+    print(s)
+
+def Devices(s):
+    print(s)
+
+def Parameters(s):
+    print(s)
