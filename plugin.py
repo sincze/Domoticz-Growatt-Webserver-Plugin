f7f56e2f229959b521bf6d8e1bfd01efd912453d
Sandor <sincze@gmail.com>
2023-08-20 22:07:22 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -81,7 +81,7 @@ except ImportError:
 
 class BasePlugin:
     httpConn = None
-    runAgain = 227          # To check every 5 minutes. 
+    runAgain = 27          # To check every 5 minutes. 
     disconnectCount = 0
     sProtocol = "HTTP"
     cookieAvailable = False
